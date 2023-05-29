7d6ec56a12e957990a5c5c570c736550b89a1e36
Leonardo Maximo Silva <leomax2002@gmail.com>
2023-05-28 21:15:34 -0300

    Verifica Erros Funcional, Teste3.asm como Exemplo

--- src/implementacao/montador.cpp
+++ src/implementacao/montador.cpp
@@ -25,9 +25,18 @@ static inline void trim(string &s) {
     }).base(), s.end());
 }
 
+//Verifica a existência de erros léxicos em Tokens
+void analisador_lexico(string arg, int cont){
+    bool f = isalpha(arg[0]);
+    if(!f && arg[0] != '_'){
+        printf("Erro Lexico na linha %d, Token invalido\n", cont);
+    }
+}
+
+
 void assemble(string filename) {
     fstream outFile, auxFile;
-    
+
     // arquivo de saída em binário
     outFile.open(filename + ".exc", ios::out | ios::trunc);
 
@@ -43,10 +52,24 @@ void assemble(string filename) {
     vector<int> mem;
     string line;
     int counter = 0;
+    //Conta as linhas para exibir nas mensagens de erro
+    int cont_linha = 0;
+    //Flags para verificar Section Text e Section Data
+    int flag_texto = 1;
+    int flag_data = 1;
+    //Flags para verificar BEGIN e END
+    int flag_begin = 1;
+    int flag_end = 1;
+    //Flags para verificar EXTERN e PUBLIC
+    int flag_extern = 0;
+    int flag_public = 0;
+    //Apenas um Alerta de Erro será mostrado
+    int erros_ditos_Data = 1;
+    int erros_ditos_Txt = 1;
 
     if (auxFile.is_open()) {
         while ( getline(auxFile, line)) {
-            
+            cont_linha++;
             stringstream ss(line);
             string word;
             vector<string> lineVec;
@@ -57,28 +80,83 @@ void assemble(string filename) {
                 string label = lineVec[0].substr(0, lineVec[0].size()-1);
                 tabSimb[label] = counter;
                 lineVec.erase(lineVec.begin());
+                //Incrementa flag_rot e verifica se há mais de um rótulo na linha
+                int numWords = lineVec.size();
+                for(int i = 0; i < numWords; i++){
+                    if(lineVec[i].back() == ':'){
+                        printf("Erro Sintatico na linha %d, dois rotulos na mesma linha\n", cont_linha);
+                        break;
+                    }
+                }
+                //Verifica Erros Léxicos no Rótulo
+                analisador_lexico(label, cont_linha);
             }
 
             string instruction = lineVec[0];
+            //cout << tabDiret[instruction] << endl;
 
             // verifica se é uma diretiva
             if (tabDiret[instruction] != 0) {
-                if (instruction == "SECTION") continue; // <- linha temporária de codigo
-                
+
+                if (instruction == "SECTION"){
+                    string tipo = lineVec[1];
+                    if(tipo == "TEXT"){
+                        flag_texto = 0;
+                    }
+                    else if(tipo == "DATA"){
+                        flag_data = 0;
+                    }
+
+                }
+
                 else if (instruction == "SPACE") {
+                        //Verifica Section DATA
+                        if(flag_data && erros_ditos_Data){
+                            erros_ditos_Data = 0;
+                            printf("Erro Semantico, sem Section DATA antes da linha %d\n", cont_linha);
+                        }
                     mem.push_back(0);
                     counter += 1;
                 }
 
                 else if (instruction == "CONST") {
+                        //Verifica Section DATA
+                        if(flag_data && erros_ditos_Data){
+                            printf("Erro Semantico, sem Section DATA antes da linha %d\n", cont_linha);
+                        }
                     mem.push_back( stoi(lineVec[1]) );
                     counter += 1;
                 }
+
+                else if (instruction == "BEGIN") {
+                        flag_begin = 0;
+
+                }
+                else if (instruction == "END") {
+                        flag_end = 0;
+                }
+                else if (instruction == "EXTERN") {
+                        flag_extern = 1;
+                        if(flag_begin) printf("Erro Semantico na linha %d, sem BEGIN\n", cont_linha);
+
+                }
+                else if (instruction == "PUBLIC") {
+                        flag_public = 1;
+                        if(flag_begin) printf("Erro Semantico na linha %d, sem BEGIN\n", cont_linha);
+
+                }
+
             }
 
             // é uma instrução do assembly
             else {
                 // coloca o opcode na memória
+                //Verifica se Section Text foi definida
+                    if(flag_texto && erros_ditos_Txt){
+                        erros_ditos_Txt = 0;
+                        printf("Erro Semantico, sem Section TEXT antes da linha %d\n", cont_linha);
+                    }
+
                 mem.push_back(tabInstr[instruction].first);
                 counter += 1;
                 int argSize = tabInstr[instruction].second;
@@ -86,19 +164,43 @@ void assemble(string filename) {
                 // adiciona linha a lista de pendencias
                 for(int i=1; i<argSize; i++) {
                     string arg = lineVec[i];
+                    //Analisador Léxico
+                    analisador_lexico(arg, cont_linha);
                     mem.push_back(-1);
                     if (!tabPend.count(arg)) tabPend[arg] = vector<int>();
                     tabPend[arg].push_back(counter);
+                    tabPend[arg].push_back(cont_linha);
+                    if(instruction == "JMP" || instruction == "JMPZ" || instruction == "JMPP" || instruction == "JMPN"){
+                        tabPend[arg].push_back(1);
+                    }
+                    else{
+                        tabPend[arg].push_back(0);
+                    }
                     counter += 1;
                 }
             }
         }
     }
 
+    //Verifica se há BEGIN e END na linha
+    if((flag_extern && flag_end) || (flag_public && flag_end)){
+            printf("Erro Semantico na linha %d, sem END\n", cont_linha);
+
+    }
+
     // arruma todas as pendências
     for(auto [lab, vec] : tabPend) {
+        int flag_erro = 0;
         for(auto idx : vec) {
             mem[idx] = tabSimb[lab];
+            if(tabSimb[lab] == 0 && vec[2] == 0 && flag_erro == 0){
+                printf("Erro Semantico, dado nao definido na linha %d\n", vec[1]);
+                flag_erro = 1;
+            }
+            if(tabSimb[lab] == 0 && vec[2] == 1 && flag_erro == 0){
+                printf("Erro Semantico, label nao definido na linha %d\n", vec[1]);
+                flag_erro = 1;
+            }
         }
     }
 
@@ -132,7 +234,11 @@ void init_fixed_tables() {
     tabDiret["CONST"] = 2;
     tabDiret["SPACE"] = 1;
     tabDiret["SECTION"] = 1;
-} 
+    tabDiret["EXTERN"] = 1;
+    tabDiret["PUBLIC"] = 1;
+    tabDiret["BEGIN"] = 1;
+    tabDiret["END"] = 1;
+}
 
 void preprocess(string filename) {
     fstream inFile, auxFile;
@@ -158,7 +264,7 @@ void preprocess(string filename) {
             auto pos = line.find(";");
             if (pos != string::npos) line = line.substr(0, pos);
 
-            // criando um espaço depois de ": " 
+            // criando um espaço depois de ": "
             auto pos3 = line.find(":");
             if (pos3 != string::npos) {
                 line.insert(pos3+1, " ");
