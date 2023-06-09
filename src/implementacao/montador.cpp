c2673d885220109e097a02bf1a4fc72476d07855
wawa <wallacebtlw@gmail.com>
2023-06-09 00:01:33 -0300

    section fix

--- src/implementacao/montador.cpp
+++ src/implementacao/montador.cpp
@@ -55,8 +55,8 @@ void init_fixed_tables() {
     tabInstr["STOP"] = {14, 1};
 
     tabDiret["CONST"] = 2;
-    tabDiret["SPACE"] = 2; // tem que aceitar args extras
-    // tabDiret["SECTION"] = 1; -> considerado no preprocessamento
+    tabDiret["SPACE"] = 2;
+    tabDiret["SECTION"] = 1;
     tabDiret["EXTERN"] = 2;
     tabDiret["PUBLIC"] = 2;
     tabDiret["BEGIN"] = 1;
@@ -153,7 +153,7 @@ void preprocess(string filename) {
         }
     }
 
-// cuidando da diretiva SECTION
+    // cuidando da diretiva SECTION
     int secDataLine = -1, secTextLine = -1;
     int file_sz = (int) fileVec.size();
 
@@ -283,9 +283,6 @@ bool assemble(string filename) {
     bool flag_text = 0;
     bool flag_data = 0;
 
-    // Mais de um Rotulo na mesma linha
-    int extraLabelCounter = 0;
-
     // Flag para saber se o arquivo gerou um objeto corretamente
     bool no_major_errors_flag = true;
 
@@ -298,6 +295,9 @@ bool assemble(string filename) {
             string lab_extern = "";
             vector<string> lineVec;
             while(ss >> word) lineVec.push_back(word);
+
+            // Mais de um Rotulo na mesma linha
+            int extraLabelCounter = 0;
 
             string label = "";
             // essa linha possui rótulo
@@ -334,9 +334,9 @@ bool assemble(string filename) {
 
                 if (instruction == "SPACE") {
 
-                    if(!flag_data){
+                    if (!flag_data){
                         printf("WARNING: (Erro Semântico) na linha %d. Sem SECTION DATA\n", lineCounter);
-                        flag_data = 1;
+                        flag_data = 1; // imprimir uma vez apenas
                     }
 
                     // SPACE possui argumento
@@ -359,8 +359,9 @@ bool assemble(string filename) {
 
                     if(!flag_data){
                         printf("WARNING: (Erro Semântico) na linha %d. Sem SECTION DATA\n", lineCounter);
-                        flag_data = 1;
+                        flag_data = 1; // imprimir uma vez apenas
                     }
+
                     if (lineVec.size() < 2) {
                         cout << "ERROR: (Erro Sintatico) na linha " << lineCounter << ". Instrucao CONST nao possui argumento." << endl;
 
@@ -434,11 +435,12 @@ bool assemble(string filename) {
                 }
 
                 else if(instruction == "SECTION"){
-                    cout << "SECTION";
+
                     if(lineVec[1] == "TEXT"){
                         flag_text = 1;
                     }
-                    if(lineVec[1] == "DATA"){
+
+                    else if(lineVec[1] == "DATA"){
                         flag_data = 1;
                     }
                 }
@@ -448,10 +450,11 @@ bool assemble(string filename) {
             // É uma instrução do assembly
             else {
 
-                  if(!flag_text){
-                        printf("ERROR: (Erro Semântico) na linha %d. Sem SECTION TEXT\n", lineCounter);
-                        flag_text = 1;
-                    }
+                if(!flag_text){
+                    printf("ERROR: (Erro Semântico) na linha %d. Sem SECTION TEXT\n", lineCounter);
+                    flag_text = 1;
+                }
+
                 // coloca o opcode na memória
                 mem.push_back(tabInstr[instruction].first);
                 fator_offset.push_back(0);
@@ -593,126 +596,6 @@ bool assemble(string filename) {
     return no_major_errors_flag;
 }
 
-void linker(vector<string> objs){
-    fstream outFile, auxFile;
-    string line;
-
-    // Tamanho Objs
-    int tamanho_objs = (int) objs.size();
-
-    // Flags
-    int fator_correcao = 0;
-    int proximo_uso = 0;
-    int proximo_def = 0;
-    int proximo_relativo = 0;
-    int proximo_codigo = 0;
-
-    //Variáveis Auxiliares
-    string arg;
-    string resp = "";
-    vector<int> cod_ligado;
-    vector<int> mem_aux;
-    //Tabela Geral de Definições e Tabela de Uso
-    unordered_map<string, int> tab_ger_def;
-    unordered_map<string,vector<int>> tab_uso;
-    vector<int> relativos;
-    //Código
-
-    //Arruma Tabelas
-    for(int i = 0; i < tamanho_objs;i++){
-        string obj = objs[i];
-        relativos.clear();
-        auxFile.open(obj+".obj",ios::in);
-
-        if(auxFile.is_open()){
-            while( getline(auxFile,line) ){
-                stringstream ss(line);
-                string word;
-                vector<string> lineVec;
-                while(ss >> word) lineVec.push_back(word);
-            if(lineVec[0] == "USO") {
-                       proximo_uso = 1;
-                       proximo_def = 0;
-                       proximo_relativo = 0;
-                       proximo_codigo = 0;
-            }
-                if(lineVec[0] == "DEF") {
-                       proximo_uso = 0;
-                       proximo_def = 1;
-                       proximo_relativo = 0;
-                       proximo_codigo = 0;
-                }
-                if(lineVec[0] == "CODE"){
-                       proximo_uso = 0;
-                       proximo_def = 0;
-                       proximo_relativo = 0;
-                       proximo_codigo = 1;
-                }
-                if(lineVec[0] == "RELATIVOS"){
-                    proximo_uso = 0;
-                    proximo_def = 0;
-                    proximo_relativo = 1;
-                    proximo_codigo = 0;
-                }
-                if(proximo_uso && lineVec[0] != "USO" && lineVec[0] != "DEF" && lineVec[0] != "CODE"){
-                   arg = lineVec[0];
-                   int val_uso = stoi(lineVec[1]) + fator_correcao;
-                   if (!tab_uso.count(arg)) tab_uso[arg] = vector<int>();
-                   tab_uso[arg].push_back(val_uso);
-            }
-            if(proximo_def && lineVec[0] != "USO" && lineVec[0] != "DEF" && lineVec[0] != "CODE" && lineVec[0] != "RELATIVOS"){
-                string arg = lineVec[0];
-                int val_uso = stoi(lineVec[1]) + fator_correcao;
-                tab_ger_def[arg] = val_uso;
-            }
-
-            if(proximo_relativo && lineVec[0] != "USO" && lineVec[0] != "DEF" && lineVec[0] != "CODE" && lineVec[0] != "RELATIVOS"){
-                for(int i = 0; i < (int)lineVec.size();i++){
-                    int id_rel = stoi(lineVec[i]);
-                    relativos.push_back(id_rel);
-                }
-
-            }
-            if(proximo_codigo && lineVec[0] != "USO" && lineVec[0] != "DEF" && lineVec[0] != "CODE" && lineVec[0] != "RELATIVOS"){
-                    proximo_codigo = 0;
-                    int contador_pos_cod = 0;
-                    for(int i = 0; i < (int)lineVec.size(); i++){
-                        int id = stoi(lineVec[i]);
-                        mem_aux.push_back(id);
-                        auto it = find(relativos.begin(), relativos.end(), contador_pos_cod);
-                        if(it != relativos.end()) id+=fator_correcao;
-                        cod_ligado.push_back(id);
-                        contador_pos_cod++;
-                    }
-                    fator_correcao = (int) lineVec.size();
-            }
-
-            }
-
-        }
-        auxFile.close();
-    }
-
-    //Resolve relativos
-    //for(auto [lab, vec])
-    for(auto [lab, vec] : tab_uso) {
-
-        for(auto idx : vec){
-
-            cod_ligado[idx] = tab_ger_def[lab] + mem_aux[idx];
-        }
-}
-
-    outFile.open(objs[0] + ".exc", ios::out | ios::trunc);
-    for(auto i : cod_ligado) resp += to_string(i) + " ";
-
-        if (outFile.is_open()) {
-            outFile << resp << '\n';
-        }
-
-    outFile.close();
-}
-
 int32_t main(int argc, char** argv) {
 
     // inicializa a tabela de instruções e a de diretivas
@@ -733,7 +616,7 @@ int32_t main(int argc, char** argv) {
             cout << "Arquivo {" << filename << "} foi montado com erros :-( " << endl;
         else
             cout << "Arquivo {" << filename << "} foi montado sem erros :-) " << endl;
+        
+        cout << endl;
     }
-
-    cout << endl;
 }
